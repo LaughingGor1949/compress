d3721182faf373a6ccd37a6b43195850e0860e44
LaughingGor1949 <LaughingGor1949@laughing.com>
2018-02-07 23:07:38 +0800

    abstract compress

---
+++ abstract_compress.cpp
@@ -0,0 +1,6 @@
+#include "abstract_compress.hpp"
+
+namespace compress{
+
+
+}
---
+++ abstract_compress.hpp
@@ -0,0 +1,18 @@
+#ifndef __COMPRESS_HPP__
+#define __COMPRESS_HPP__
+
+#include <fstream>
+
+namespace compress{
+
+
+class AbstractCompress
+{
+
+};
+
+
+
+};/*end namespace compress*/
+
+#endif/*end if*/
