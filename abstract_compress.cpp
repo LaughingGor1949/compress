e1334bcd81d47f9d4fd2fe9352d6c96f90d33b55
LaughingGor1949 <LaughingGor1949@laughing.com>
2018-02-10 17:53:47 +0800

    update abstract compress

--- abstract_compress.cpp
+++ abstract_compress.cpp
@@ -1,6 +1,62 @@
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <glog/logging.h>
 #include "abstract_compress.hpp"
 
-namespace compress{
+using namespace std;
 
+namespace compress{
+cpsStatus AbstractCompress::getSparseData(uint8_t* inputData, uint32_t inputLen, uint8_t sparseRate)
+{
+    if (inputData == NULL)
+    {
+        LOG(WARNING) << "input point is NULL!";
+        return STATUS_BAD_PARAM;
+    }
+    if ((sparseRate == SPARSE_RATE_MIN) || (sparseRate >= SPARSE_RATE_MAX))
+    {
+        LOG(WARNING) << "sparse rate = " << sparseRate << " should be in (0, 100)!";
+        return STATUS_BAD_PARAM;
+    }
+    srand(static_cast<int>(time(NULL)));
+    uint32_t randDataMax = getCeil(UINT8_VALUE_MAX * SPARSE_RATE_MAX, SPARSE_RATE_MAX - sparseRate);
+    uint32_t randData = 0;
+    for (uint32_t i = 0; i < inputLen; ++i)
+    {
+        randData = rand() %  (randDataMax + 1);
+        if (randData >= UINT8_VALUE_MAX)
+        {
+            inputData[i] = 0;
+        }
+        else
+        {
+            inputData[i] = static_cast<uint8_t>(randData);
+        }
+    }
+    LOG(INFO) << "data size is " << inputLen << " bytes";
+    LOG(INFO) << "data sparse is " << static_cast<int>(sparseRate) << "%";
+    return STATUS_SUCCESS;
+}
+
+
+cpsStatus AbstractCompress::out2BinFile(const char* inputData, uint32_t inputLen, string fileName)
+{
+    if (inputData == NULL)
+    {
+        LOG(WARNING) << "input point is NULL!";
+        return STATUS_BAD_PARAM;
+    }
+
+    ofstream ofile(fileName, ios::binary);
+    if (!ofile)
+    {
+        LOG(WARNING) << "open " << fileName << " fail!";
+        return STATUS_RUNTIME_ERROR;
+    }
+    ofile.write(inputData, inputLen);
+    ofile.close();
+    return STATUS_SUCCESS;
+}
 
 }
--- abstract_compress.hpp
+++ abstract_compress.hpp
@@ -1,18 +1,47 @@
-#ifndef __COMPRESS_HPP__
-#define __COMPRESS_HPP__
+#ifndef __ABSTRACT_COMPRESS_HPP__
+#define __ABSTRACT_COMPRESS_HPP__
 
-#include <fstream>
+#include <stdint.h>
+#include <string>
 
 namespace compress{
 
-
-class AbstractCompress
+enum compressStatus
 {
+    STATUS_SUCCESS = 0,             /* succ */
+    STATUS_NOT_INITIALIZED,         /* not initialize */
+    STATUS_ALLOC_FAILED,            /* malloc failed */
+    STATUS_BAD_PARAM,               /* check para failed */
+    STATUS_INTERNAL_ERROR,          /* internal error */
+    STATUS_RUNTIME_ERROR,           /* runtime error */
+    STATUS_NOT_SUPPORTED,           /* not supported */
+    STATUS_RESERVED                 /* reserved */
+};
+using cpsStatus = compressStatus;
 
+
+enum
+{
+    CHAR_BIT = 8,
+    CHAR_SIZE = 256,
+    UINT8_VALUE_MAX = 255,
+    SPARSE_RATE_MAX = 100,
+    SPARSE_RATE_MIN = 0
 };
 
 
+inline uint32_t getCeil(uint32_t numerator, uint32_t denominator)
+{
+    return (numerator + denominator - 1) / denominator;
+}
 
-};/*end namespace compress*/
+class AbstractCompress
+{
+public:
+    static cpsStatus getSparseData(uint8_t* inputData, uint32_t inputLen, uint8_t sparseRate = 80);
+    static cpsStatus out2BinFile(const char* inputData, uint32_t inputLen, std::string fileName);
+};
 
-#endif/*end if*/
+};/* end namespace compress */
+
+#endif/* end if */
