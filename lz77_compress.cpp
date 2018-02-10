564de350b1dbf0088038363892e5dc8324556195
LaughingGor1949 <LaughingGor1949@laughing.com>
2018-02-10 22:31:23 +0800

    lz77 compress

--- lz77_compress.cpp
+++ lz77_compress.cpp
@@ -1,5 +1,16 @@
 #include "lz77_compress.hpp"
 
 namespace compress{
-    
+
+Lz77Compress::Lz77Compress()
+{
+
+}
+
+cpsStatus Lz77Compress::lz77Compress(const uint8_t* inputData, uint32_t inputLen, uint8_t* outputData, uint32_t& outputLen)
+{
+    uint8_t windowData[LZ77_WINDOW_SIZE] = {0};
+    uint8_t buffData[LZ77_BUFF_SIZE] = {0};
+}
+
 }
--- lz77_compress.hpp
+++ lz77_compress.hpp
@@ -1,7 +1,39 @@
 #ifndef __LZ77_COMPRESS_HPP__
 #define __LZ77_COMPRESS_HPP__
 
-namespace compress{
+#include "abstract_compress.hpp"
+
+namespace compress
+{
+
+class Lz77Compress
+{
+
+struct LZ77
+{
+    uint8_t* windowData;
+    uint8_t* buffData;
+    uint32_t curson;
+    uint32_t length;
+    uint8_t nextData;
+};
+
+
+public:
+Lz77Compress();
+cpsStatus lz77Compress(const uint8_t* inputData, uint32_t inputLen, uint8_t* outputData, uint32_t& outputLen);
+
+protected:
+const static uint32_t LZ77_TYPE_BITS = 1;
+const static uint32_t LZ77_WINOFF_BITS = 12;
+const static uint32_t LZ77_BUFFLEN_BITS = 5;
+const static uint32_t LZ77_NEXT_BITS = 8;
+const static uint32_t LZ77_WINDOW_SIZE = 4096;
+const static uint32_t LZ77_BUFF_SIZE = 32;
+const static uint32_t LZ77_PHRASE_BITS = LZ77_TYPE_BITS + LZ77_WINOFF_BITS + LZ77_NEXT_BITS + LZ77_BUFFLEN_BITS;
+const static uint32_t LZ77_SYMBOL_BITS = LZ77_TYPE_BITS + LZ77_NEXT_BITS;
+};
+
 
 };
 
